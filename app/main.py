c50c6261c6d5bb20c487777c4f86a49a6d2af798
ArthisreeR <arthisree.r@bakerhughes.com>
2024-12-16 13:54:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,9 +15,13 @@ from app.statparam import stat
 app = FastAPI(debug=True)
 @app.post("/stats/")
 async def statfunction( input : str = Form(...)):
-     json_data = ast.literal_eval(input)
-     result = stat(json_data)
-     return result
+   try:
+       json_data = ast.literal_eval(input)
+       result = stat(json_data)
+       return result
+   except Exception as e:
+       return {"error": str(e)}
+      
  
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8007)
