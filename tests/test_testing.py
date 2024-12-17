dae0cefb12adc1116ecd18f7ed58bbdf30c87cbf
ArthisreeR <arthisree.r@bakerhughes.com>
2024-12-17 18:42:23 +0530

    Update test_testing.py

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,21 +1,18 @@
 import json
 import pytest
 from fastapi.testclient import TestClient
-
-from api.main import app
  
+from api.main import app
 @pytest.fixture
 def input_data():
     # Load the input data from the JSON file
     with open('tests/input_data.json') as f:
         return json.load(f)  
- 
 @pytest.fixture
 def client():
     with TestClient(app) as client:
         yield client
- 
-def test_statistics_api(client, input_data):
+def test_stat_api(client, input_data):
     # Send a POST request to /stats/ with the input data read from the file
     response = client.post(
         '/stats/', 
@@ -41,18 +38,12 @@ def test_statistics_api(client, input_data):
     assert result["stdlimits"]["1_std_limits"] == [57.50433862037556, 114.01566137962445]
     assert result["stdlimits"]["2_std_limits"] == [29.248677240751128, 142.27132275924887]
     assert result["stdlimits"]["3_std_limits"] == [0.9930158611266933, 170.52698413887333]
-
-def invalidinput_data():
-    # Load the invalidinput data from the JSON file
-    with open('tests/invalidinput_data.json') as d:
-        return json.load(d)  
- 
-def test_invalid_data(client,invalidinput_data):
+def test_invalid_data(client):
     # Send a POST request with invalid input (e.g., non-numeric data)
     response = client.post(
         '/stats/',
-        json= invalidinput_data # Invalid data
+        json={"data": ["a", "b", "c"]}  # Invalid data
     )
     # Check if the status code is 422 Unprocessable Entity (for invalid input)
     assert response.status_code == 422
-    assert "details" in response.json()
+    assert "detail" in response.json()
