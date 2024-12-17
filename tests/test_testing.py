fb5ed6ff5bf205364d499015e149f30b323e28d2
ArthisreeR <arthisree.r@bakerhughes.com>
2024-12-17 10:29:32 +0530

    Update test_testing.py

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,9 +1,7 @@
 import json
 import pytest
 from fastapi.testclient import TestClient
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../api')))
+
 from api.main import app
  
 @pytest.fixture
