582e9c8198c6dc5f5c81bae8ee1fe82c012401fe
ArthisreeR <arthisree.r@bakerhughes.com>
2024-12-18 12:55:35 +0530

    Update test_testing.py

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -38,6 +38,32 @@ def test_stat_api(client, input_data):
     assert result["stdlimits"]["1_std_limits"] == [57.50433862037556, 114.01566137962445]
     assert result["stdlimits"]["2_std_limits"] == [29.248677240751128, 142.27132275924887]
     assert result["stdlimits"]["3_std_limits"] == [0.9930158611266933, 170.52698413887333]
+def test_wrongoutput_api(client, input_data):
+    # Send a POST request to /stats/ with the input data read from the file
+    response = client.post(
+        '/stats/', 
+        json=input_data  
+    )
+    # Check if the status code is 200 (OK)
+    assert response.status_code == 200
+    # Parse the JSON response
+    result = response.json()
+    # Assertions based on the expected behavior
+    assert "meanval" in result
+    assert "medianval" in result
+    assert "IQR" in result
+    assert "Lowerlimit" in result
+    assert "upperlimit" in result
+    assert "stdlimits" in result
+    # Compare the returned values with the expected ones
+    assert result["meanval"] == 85.75
+    assert result["medianval"] == 89.4
+    assert result["IQR"] == 38.23
+    assert result["Lowerlimit"] == 9.872
+    assert result["upperlimit"] == 162.876
+    assert result["stdlimits"]["1_std_limits"] == [57.50433862037557, 114.01566137962445]
+    assert result["stdlimits"]["2_std_limits"] == [29.248677240751127, 142.27132275924887]
+    assert result["stdlimits"]["3_std_limits"] == [0.9930158611266935, 170.52698413887333]
 def test_invalid_data(client):
     # Send a POST request with invalid input (e.g., non-numeric data)
     response = client.post(
