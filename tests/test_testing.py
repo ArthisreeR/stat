92c0eb8656674462445543f8a17e407ef871c8b1
ArthisreeR <arthisree.r@bakerhughes.com>
2024-12-24 20:37:29 +0530

    Update test_testing.py

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -20,7 +20,7 @@ def client():
         yield client
 def test_stat_api(client, input_data):
     # Send a POST request to /stats/ with the input data read from the file
-    response = client.post('/stats/', data={'input': json.dumps(input_data)})
+    response = client.post('/stats/', json=json.dumps(input_data))
     assert response.status_code == 200
     result = response.json()
     # Assertions based on the expected behavior
@@ -41,7 +41,7 @@ def test_stat_api(client, input_data):
     assert result["stdlimits"]["3_std_limits"] == [0.9930158611266933, 170.52698413887333]
 def test_wrongoutput_api(client, input_data):
     # Send a POST request to /stats/ with the input data read from the file
-    response = client.post('/stats/', data={'input': json.dumps(input_data)})
+    response = client.post('/stats/', json=json.dumps(input_data)))
     assert response.status_code == 200
     result = response.json()
     # Assertions based on the expected behavior
@@ -62,7 +62,7 @@ def test_wrongoutput_api(client, input_data):
     assert result["stdlimits"]["3_std_limits"] == [0.9930158611266935, 170.52698413887333]
 def test_invalid_data(client, wrong_data):
     # Send a POST request with invalid input (e.g., non-numeric data)
-    response = client.post( '/stats/',data={'input': wrong_data})  
+    response = client.post( '/stats/',json=json.dumps(wrong_data)))  
     # Check if the status code is 422 Unprocessable Entity (for invalid input)
     assert response.status_code == 422
     assert "detail" in response.json()
