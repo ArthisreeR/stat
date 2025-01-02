c08361060881d0bf5deec0cfa0c97225371a81dc
ArthisreeR <arthisree.r@bakerhughes.com>
2025-01-02 16:01:58 +0530

    Update test_testing.py

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -13,6 +13,16 @@ def input_data():
 def output_data():
     with open('tests/output_data.json') as a:
         return json.load(a)  
+@pytest.fixture
+def alterinput_data():
+    # Load the input data from the JSON file
+    with open('tests/input_data1.json') as s:
+        return json.load(s)  
+
+@pytest.fixture
+def alteroutput_data():
+    with open('tests/output_data1.json') as e:
+        return json.load(e) 
      
 @pytest.fixture
 def wrong_data():
@@ -47,6 +57,28 @@ def test_stat_api(client, input_data,output_data):
     assert result["stdlimits"]["1_std_limits"] == output_data["stdlimits"]["1_std_limits"]
     assert result["stdlimits"]["2_std_limits"] == output_data["stdlimits"]["2_std_limits"]
     assert result["stdlimits"]["3_std_limits"] == output_data["stdlimits"]["3_std_limits"]
+
+def test_alter_test(client,alterinput_data,alteroutput_data):
+    # Send a POST request to /stats/ with the input data read from the file
+    response = client.post('/stats/', data = {"input" : alterinput_data })
+    assert response.status_code == 200
+    result = response.json()
+    # Assertions based on the expected behavior
+    assert "meanval" in result
+    assert "medianval" in result
+    assert "IQR" in result
+    assert "Lowerlimit" in result
+    assert "upperlimit" in result
+    assert "stdlimits" in result
+    # Compare the returned values with the expected ones
+    assert result["meanval"] == alteroutput_data["meanval"]
+    assert result["medianval"] == alteroutput_data["medianval"]
+    assert result["IQR"] == alteroutput_data["IQR"]
+    assert result["Lowerlimit"] == alteroutput_data["Lowerlimit"]
+    assert result["upperlimit"] == alteroutput_data["upperlimit"]
+    assert result["stdlimits"]["1_std_limits"] == alteroutput_data["stdlimits"]["1_std_limits"]
+    assert result["stdlimits"]["2_std_limits"] == alteroutput_data["stdlimits"]["2_std_limits"]
+    assert result["stdlimits"]["3_std_limits"] == alteroutput_data["stdlimits"]["3_std_limits"]
 
 def test_invalid_data(client, wrong_data):
     # Send a POST request with invalid input (e.g., non-numeric data)
