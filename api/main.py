4092df9f912ff62db564a04c16abd4832fa350e7
ArthisreeR <arthisree.r@bakerhughes.com>
2025-01-03 13:09:25 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -31,4 +31,4 @@ async def statfunction( input : str = Form(...)):
      return result
  
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8008)
+    uvicorn.run(app, host="127.0.0.1", port=9130)
